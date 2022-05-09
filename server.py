883347946830abe3be45848849ba92648e0874a6
1565479 - Jose A. Ramos Andrades <joseantonio.ramosa@autonoma.cat>
2022-05-09 18:38:44 +0200

    Added list conversion

--- server.py
+++ server.py
@@ -37,8 +37,8 @@ async def time(websocket, path):
                 offers.append([(True if offer["remote"] == 1 else 0), offer["salary"], offer["labour"], [
                     s for s in offer["skills"]], [s for s in offer["coords"]], offer["id"]])
 
-            await websocket.send(str(Expert(user, offers)))
-            # await websocket.send(json.dumps({'offer': offer}))
+            # await websocket.send(str(Expert(user, offers)))
+            await websocket.send(json.dumps(Expert(user, offers).tolist()))
         print("offer received")
 
     if path == '/getI2I_offers':
@@ -47,7 +47,8 @@ async def time(websocket, path):
             full_json = json.loads(message)
             user_json = full_json['user']
             user_json["distance"][0] = -np.infty if user_json["distance"][0] == 1.4E-45 else user_json["distance"][0]
-            user_json["distance"][1] = np.infty if user_json["distance"][1] == 3.4028235E38 else user_json["distance"][1]
+            user_json["distance"][1] = np.infty if user_json["distance"][1] == 3.4028235E38 else user_json["distance"][
+                1]
             user_json["salary"][0] = -np.infty if user_json["salary"][0] == 1.4E-45 else user_json["salary"][0]
             user_json["salary"][1] = np.infty if user_json["salary"][1] == 3.4028235E38 else user_json["salary"][1]
             user_json["labour"][0] = -np.infty if user_json["labour"][0] == 1.4E-45 else user_json["labour"][0]
@@ -76,7 +77,9 @@ async def time(websocket, path):
 
             await websocket.send(str(item2item(user, liked, offers)))
             # await websocket.send(json.dumps({'offer': offer}))
+
         print("offer received")
+
 
 start_server = websockets.serve(time, '127.0.0.1', 4321)
 asyncio.get_event_loop().run_until_complete(start_server)
