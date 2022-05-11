5f9fe580ce1edc7e734613b46b848b65a46c691a
maymac00 <arnau.mayoral10@gmail.com>
2022-05-11 12:53:36 +0200

    dont repeat api calls

--- GeoUtils.py
+++ GeoUtils.py
@@ -7,7 +7,7 @@ import haversine as hs
 class GeoUtils:
     data = pgeocode.Nominatim('ES')
     distance = pgeocode.GeoDistance('es')
-
+    pc2coords = {}
     @staticmethod
     def get_distance(lat1, lon1, lat2, lon2):
         return hs.haversine((lat1, lon1), (lat2, lon2))
@@ -19,5 +19,11 @@ class GeoUtils:
 
     @staticmethod
     def get_coords_from_postal_code(postal_code):
-        city = GeoUtils.data.query_postal_code(postal_code)
-        return city.latitude, city.longitude
+
+        if postal_code not in GeoUtils.pc2coords.keys():
+            city = GeoUtils.data.query_postal_code(postal_code)
+            GeoUtils.pc2coords[postal_code] = (city.latitude, city.longitude)
+            return city.latitude, city.longitude
+        else:
+            t = GeoUtils.pc2coords[postal_code]
+            return t[0], t[1]
